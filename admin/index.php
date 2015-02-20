4fbd9d5dae3072f1eb66a1f47c8802f5fd8baac2
Berwout Kruit <tehber@hotmail.com>
2015-02-20 13:19:41 +0100

    Basis admin panel toegevoegd.

---
+++ admin/index.php
@@ -0,0 +1,9 @@
+<?php
+
+if (/*User::hasAuth('admin')*/1){
+	//
+	include('main.php');
+} else {
+	//
+	header('Location: HTTP://'.$_SERVER['HTTP_HOST']);
+}
---
+++ admin/main.php
@@ -0,0 +1,29 @@
+<html lang="nl">
+	<head>
+		<title>Admin Paneel - Webshopping</title>
+	</head>
+
+	<body>
+		<div id="admin-panel">
+			Admin paneel
+
+			<div class="button">
+				Producten
+				<div class="hover-button">
+					Toevoegen
+				</div>
+
+				<div class="hover-button">
+					Verwijderen
+				</div>
+			</div>
+
+			<div class="button">
+				Gebruikers
+				<div class="hover-button">
+					Informatie
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
