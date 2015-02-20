75b1d239a02015c311a314c3ccf3505898a72f41
JohJohan <johan.vlaar.1994@gmail.com>
2015-02-20 13:17:46 +0100

    Head gemaakt met bootstrap

---
+++ resources/head.php
@@ -0,0 +1,25 @@
+<head>
+	<title>Webshopping</title>
+    <link rel="shortcut icon" href="/webshopping/img/logo.png">
+	
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Bootstrap JavaScript -->
+    <script src="/webshopping/libs/bootstrap.min.js"></script>
+    
+    <!-- Custom JavaScript -->
+
+	<!-- Bootstrap CSS -->
+	<link href="/webshopping/css/bootstrap.min.css"			rel="stylesheet" media="screen">
+
+	<!-- Custum CSS -->
+	<link href="/webshopping/css/main.css"           		rel="stylesheet" media="screen">
+
+	<!-- Custom Phone CSS -->
+	<link href="/webshopping/css/phone/main.css"           	rel="stylesheet" media="screen">
+
+</head>
