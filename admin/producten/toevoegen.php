4207e80d1f92f8c363ea65577cf0799a7cc91d1e
Berwout Kruit <tehber@hotmail.com>
2015-03-06 14:27:07 +0100

    Admin panel bijna klaar

--- admin/producten/toevoegen.php
+++ admin/producten/toevoegen.php
@@ -1,2 +1,37 @@
-<?php
-echo 'het werkt';
+<?php include('../libs/admin/category.php'); ?>
+
+<div id="function">
+	<div class="title">
+		Product toevoegen
+	</div>
+
+	<table>
+		<tr>
+			<td>Product Naam</td>
+			<td><input type="text"></td>
+		</tr>
+		<tr>
+			<td>Product Code</td>
+			<td><input type="text"></td>
+		</tr>
+		<tr>
+			<td>Product Prijs</td>
+			<td><input type="text"></td>
+		</tr>
+		<tr>
+			<td>Product Category</td>
+			<td>
+				<select name="" id="">
+					<?php category::make(); ?>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td>Product Omschrijving</td>
+			<td><textarea name="" id="" cols="30" rows="10"></textarea></td>
+		</tr>
+		<tr>
+			<td><input type="submit" value="Toevoegen"></td>
+		</tr>
+	</table>
+</div>
