06414438ec0e733032dc8c9043382ca1eeccafe2
Klaasjan <klaasjanoosterveld1995@gmail.com>
2015-03-24 13:32:34 +0100

    Login try to do this

--- login_redir.php
+++ login_redir.php
@@ -2,8 +2,12 @@
 	include_once 'libs/auth.php';
 		empty($_POST["username"]);
 		empty($_POST["password"]);
-		
+		header('Location: http://'.$_SERVER['SERVER_NAME'].'/webshopping/');
+
 		if (Auth::login($naam, $wachtwoord) == false){
 			echo 'je moeder';
 		}
+		else
+		{
 		var_dump($_POST);
+		}
