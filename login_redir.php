11a37db9d51508b4c0938fbb7bf440a68e3cc6ab
Berwout Kruit <tehber@hotmail.com>
2015-03-25 09:16:20 +0100

    Comments er bij gezet

--- login_redir.php
+++ login_redir.php
@@ -1,13 +1,15 @@
 <?php
 	include_once 'libs/auth.php';
-		empty($_POST["username"]);
-		empty($_POST["password"]);
-		header('Location: http://'.$_SERVER['SERVER_NAME'].'/webshopping/');
+		empty($_POST["username"]);//Wel een 'check' schrijven maar er niks mee doen?
+		empty($_POST["password"]);//Zelfde hier, het returnd true of false maar wat doe je er mee?
+		header('Location: http://'.$_SERVER['SERVER_NAME'].'/webshopping/'); //Hier mee worden we direct terug gestuurt nog
+		//voordat er ook maar iets gebeurt
 
-		if (Auth::login($naam, $wachtwoord) == false){
-			echo 'je moeder';
+		if (Auth::login($naam, $wachtwoord) == false){ //Legit check werkt.
+			echo 'je moeder';//Waarom :V
 		}
 		else
 		{
-			Auth::login($naam, $wachtwoord);
+			Auth::login($naam, $wachtwoord); //Waarom roepen we login nog een keer aan? Als het goed gaat doet het alles al.
+			//Dit is een beetje erg overbodig. En ik twijfel of dit wel uitgevoerd wordt.
 		}
