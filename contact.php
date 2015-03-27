a008552e619228f0607e9352dafa18e73db5dc79
johan vlaar <johan.vlaar.1994@gmail.com>
2015-03-27 15:00:16 +0100

    Side menu phone css

--- contact.php
+++ contact.php
@@ -9,54 +9,64 @@
 
     <div class="container content">
     	<div class="container-fluid">
-    		<div class="row">
-    			<div class="col-md-6">
-    			<h1>Over webshopping</h1>
-    			<hr>
-    			<p>Hallo, wij zijn <strong>Johan, Berwout en Klaasjan</strong></p>
-    			<p>Wij zijn webshopping begonnen voor .... om de verkopen ook online te doen, met een verfrissend tintje.</p>
-    			</div>
-    			<div class="col-md-6">
-    				<h1>Hoofdschotjes</h1>
-    				<hr>
-                    <div class="col-md-4">
-    					<label><img src="img/johan.jpg" alt="Johan" class="img-circle" style="height:100px;"><p>Johan Vlaar</p></label>
-    				</div>
-                    <div class="col-md-4">    
-                        <label><img src="img/berwout.jpg" alt="Berwout" class="img-circle" style="height:100px;"><p>Berwout Kruit</p> </label>
-    				</div>
-                    <div class="col-md-4">    
-                        <label><img src="img/klaasjan.jpg" alt="Klaasjan" class="img-circle" style="height:100px;"> <p>Klaasjan Oosterveld</p> </label>
-    			     </div>
-                </div>
-    		</div>
-    		<div class="row">
-        			<div class="col-md-6">
-	        			<h1>Openingstijden</h1>
-	        			<hr>
-	                    <ul>
-	                        <li>Webshop 24/7</li>
-	                        <li>Maandag     09:00 - 20:00</li>
-	                        <li>Dinsdag     09:00 - 20:00</li>
-	                        <li>woesdag     09:00 - 20:00</li>
-	                        <li>Donderdag   09:00 - 22:00</li>
-	                        <li>vrijdag     09:00 - 20:00</li>
-	                        <li>Zaterdag    13:00 - 18:00</li>
-	                        <li>Zondag      13:00 - 17:00</li>
-	                    </ul>
-                    </div>
-                    <div class="col-md-6">
-                    	<h1>Waar zijn wij te vinden?</h1>
-                    	<hr>
-                    	<ul>
-                    		<li>adress: Travertijnstraat 6</li>
-                    		<li>postcode: 9743 SZ Groningen</li>
-                    	</ul>
-                    	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4777.893791122629!2d6.5247440947663335!3d53.218800512620874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x38fe736a72f7aa5e!2sTechnisch+%26+Creatief+Jeugdcentrum+stichting+SCN!5e0!3m2!1snl!2snl!4v1424867556607" frameborder="0" style="border:0" height="400px;" width="500px;"></iframe>
+            <div class="row">
 
+                <div class="col-md-6">
+
+                    <div class="col-md-12">
+                        <h1>Openingstijden</h1>
+                        <hr>
+                        <ul>
+                            <li>Webshop 24/7</li>
+                            <li>Maandag     09:00 - 20:00</li>
+                            <li>Dinsdag     09:00 - 20:00</li>
+                            <li>woesdag     09:00 - 20:00</li>
+                            <li>Donderdag   09:00 - 22:00</li>
+                            <li>vrijdag     09:00 - 20:00</li>
+                            <li>Zaterdag    13:00 - 18:00</li>
+                            <li>Zondag      13:00 - 17:00</li>
+                        </ul>
                     </div>
-        		</div>
-        	</div>
+                    
+                    <div class="col-md-12">
+                        <h1>Waar zijn wij te vinden?</h1>
+                        <hr>
+                        <ul>
+                            <li>adress: Travertijnstraat 6</li>
+                            <li>postcode: 9743 SZ Groningen</li>
+                        </ul>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4777.893791122629!2d6.5247440947663335!3d53.218800512620874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x38fe736a72f7aa5e!2sTechnisch+%26+Creatief+Jeugdcentrum+stichting+SCN!5e0!3m2!1snl!2snl!4v1424867556607" frameborder="0" style="border:0" height="400px;" width="500px;"></iframe>
+                    </div>
+
+                </div>
+                <div class="col-md-6">
+
+                    <div class="col-md-12">
+                        <h1>Over webshopping</h1>
+                        <hr>
+                        <p>Hallo, wij zijn <strong>Johan, Berwout en Klaasjan</strong></p>
+                        <p>Wij zijn webshopping begonnen voor .... om de verkopen ook online te doen, met een verfrissend tintje.</p>
+                    </div>
+                   
+                    <div class="col-md-12">
+                        <h1>Hoofdschotjes</h1>
+                        <hr>
+                        <div class="col-md-12">
+                            <label><img src="img/johan.jpg" alt="Johan" class="img-circle" style="height:100px;"><p>Johan Vlaar</p></label>
+                            <p class="overPersoon"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus eius odit, corporis ex unde ipsam consequatur sit numquam error vel facere, nesciunt, sapiente odio non placeat accusamus repellat hic dolorem.</p>
+                        </div>
+                        <div class="col-md-12">    
+                            <label><img src="img/berwout.jpg" alt="Berwout" class="img-circle" style="height:100px;"><p>Berwout Kruit</p> </label>
+                            <p class="overPersoon">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore mollitia placeat facilis blanditiis, debitis perferendis deserunt nihil doloribus repudiandae praesentium veniam, iusto non et sed commodi obcaecati. Ipsam natus, perspiciatis!</p>
+                        </div>
+                        <div class="col-md-12">    
+                            <label><img src="img/klaasjan.jpg" alt="Klaasjan" class="img-circle" style="height:100px;"> <p>Klaasjan Oosterveld</p> </label>
+                            <p class="overPersoon">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste nemo molestias ut perferendis quo modi cupiditate est consectetur voluptate voluptatem tenetur officiis ad at, sapiente dignissimos harum, ullam dicta quas!</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
        	</div>
     </div>
 
---
+++ resources/sidemenu.php
@@ -0,0 +1,53 @@
+<div class="sideMenu hidden-xs">
+	<ul>
+		<li><h4><a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/catogorie.php'?>>CATEGORIEËN</a></h4></li>
+		<hr>
+
+		<li class="firstLi">Hardware</li>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/geheugen.php'?>><li class="secondLi">Geheugen</li></a>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/controllers.php'?>><li class="secondLi">Controllers</li></a>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/voeding.php'?>><li class="secondLi">Voeding</li></a>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/videokaarten.php'?>><li class="secondLi">Videokaarten</li></a>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/moederbord.php'?>><li class="secondLi">Moederbord</li></a>
+
+		<li class="firstLi">Opslag</li>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/hardeschrijf.php'?>><li class="secondLi">Hardeschrijven</li> </a>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/ssd.php'?>><li class="secondLi">SSD</li></a>
+
+		<li class="firstLi" >Koeling</li>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/fans.php'?>><li class="secondLi">Fans</li></a>
+
+		<li class="firstLi">Casemodding</li>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/behuizing.php'?>><li class="secondLi">Behuizing</li></a>
+	</ul>
+</div>
+<a class="btn btn-primary visible-xs" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+  Show categorieën
+</a>
+<div class="collapse" id="collapseExample">
+  <div class="well">
+    <div class="sideMenu">
+	<ul>
+		<li><h4>CATEGORIEËN</h4></li>
+		<hr>
+
+		<li class="firstLi">Hardware</li>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/geheugen.php'?>><li class="secondLi">Geheugen</li></a>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/controllers.php'?>><li class="secondLi">Controllers</li></a>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/voeding.php'?>><li class="secondLi">Voeding</li></a>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/videokaarten.php'?>><li class="secondLi">Videokaarten</li></a>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/moederbord.php'?>><li class="secondLi">Moederbord</li></a>
+
+		<li class="firstLi">Opslag</li>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/hardeschrijf.php'?>><li class="secondLi">Hardeschrijven</li> </a>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/ssd.php'?>><li class="secondLi">SSD</li></a>
+
+		<li class="firstLi" >Koeling</li>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/fans.php'?>><li class="secondLi">Fans</li></a>
+
+		<li class="firstLi">Casemodding</li>
+			<a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/webshopping/producten/behuizing.php'?>><li class="secondLi">Behuizing</li></a>
+	</ul>
+</div>
+  </div>
+</div>
